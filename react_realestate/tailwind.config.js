2f95cb50618f1d5bbc5342af76f8e63d3299cf37
Wesley Jiang <wjiang917@gmail.com>
2024-06-02 17:04:58 -0600

    Installed and configured Tailwind CSS and NextUI libraries

---
+++ react_realestate/tailwind.config.js
@@ -0,0 +1,25 @@
+import { nextui } from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './src/**/*.{html,js,jsx,ts,tsx}',
+  "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {},
+};
+export const darkMode = "class";
+export const plugins = [nextui({
+  themes: {
+    light: {
+      colors: {
+        primary: {
+          DEFAULT: '#1E1E1E',
+        },
+      },
+    },
+    dark: {
+      colors: {},
+    },
+  },
+})];
