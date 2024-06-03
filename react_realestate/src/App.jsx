b394c15dde031be1e3483b695de94c82ebaf5f62
Wesley Jiang <wjiang917@gmail.com>
2024-06-03 11:17:49 -0600

    Updated App component with MainContext and additional routes

--- react_realestate/src/App.jsx
+++ react_realestate/src/App.jsx
@@ -1,13 +1,25 @@
 import { Route, Routes } from 'react-router-dom';
+import MainContext from './components/Main/MainContext';
 import Login from "./pages/Login";
+import Main from './pages/Main';
+import Customer from './components/Main/Customer';
 
 function App() {
+  const mainData = {
+    titles: {
+      customers: 'Clientes',
+    },
+  };
   return (
     <>
-      <Routes>
-        <Route path='/' element={<Login />}>
-        </Route>
-      </Routes>
+      <MainContext.Provider value={mainData}>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/main/*' element={<Main />}>
+            <Route path='customer' element={<Customer />} />
+          </Route>
+        </Routes>
+      </MainContext.Provider>
     </>
   )
 }
