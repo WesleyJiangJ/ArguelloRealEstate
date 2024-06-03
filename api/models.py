36a61e080038cd652b8c2c47f352dd581a6aabc3
Wesley Jiang <wjiang917@gmail.com>
2024-06-03 17:22:27 -0600

    Added Customer model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    first_surname = models.CharField(max_length=30)
+    second_surname = models.CharField(max_length=30)
+    birthdate = models.DateField()
+    phone_number = models.CharField(max_length=8)
+    email = models.EmailField(max_length=255, unique=True, blank=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.middle_name} {self.first_surname} {self.second_surname}"
