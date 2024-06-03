ba6dab09d53f56bdc810324d97a1873366f6a932
Wesley Jiang <wjiang917@gmail.com>
2024-06-03 17:24:51 -0600

    Added CustomerViewSet to router path for API endpoint registration

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.documentation import include_docs_urls
 from api import views
 
 router = routers.DefaultRouter()
+router.register(r"customer", views.CustomerViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
