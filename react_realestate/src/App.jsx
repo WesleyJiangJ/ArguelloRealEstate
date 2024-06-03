27c141c4377485d5c31ed54a4cc0b540d7e247eb
Wesley Jiang <wjiang917@gmail.com>
2024-06-03 10:19:28 -0600

    react-router-dom library was implemented

--- react_realestate/src/App.jsx
+++ react_realestate/src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from "./pages/Login";
+
 function App() {
   return (
     <>
-      
+      <Routes>
+        <Route path='/' element={<Login />}>
+        </Route>
+      </Routes>
     </>
   )
 }
