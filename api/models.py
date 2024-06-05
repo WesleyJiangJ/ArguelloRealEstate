88c2a1de484b4bae2cfb857c70e4ae84932f74af
Wesley Jiang <wjiang917@gmail.com>
2024-06-05 11:04:16 -0600

    Added DNI field with validation to Customer model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 
 class Customer(models.Model):
@@ -7,6 +8,7 @@ class Customer(models.Model):
     first_surname = models.CharField(max_length=30)
     second_surname = models.CharField(max_length=30)
     birthdate = models.DateField()
+    dni = models.CharField(max_length=16, validators=[MinLengthValidator(16)])
     phone_number = models.CharField(max_length=8)
     email = models.EmailField(max_length=255, blank=True)
     status = models.BooleanField(default=True)
