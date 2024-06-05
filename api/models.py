7844e7fc32fe45838978e440174bd41aef282b6a
Wesley Jiang <wjiang917@gmail.com>
2024-06-05 13:47:12 -0600

    modified_at field was added in Customer model

--- api/models.py
+++ api/models.py
@@ -12,6 +12,7 @@ class Customer(models.Model):
     phone_number = models.CharField(max_length=8)
     email = models.EmailField(max_length=255, blank=True)
     status = models.BooleanField(default=True)
+    modified_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
