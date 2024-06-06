ec99b7b5e6170c09065ef538f628ae9f99b969d7
Wesley Jiang <wjiang917@gmail.com>
2024-06-06 09:02:55 -0600

    Added NotesSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -6,3 +6,9 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = "__all__"
+
+
+class NotesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notes
+        fields = "__all__"
