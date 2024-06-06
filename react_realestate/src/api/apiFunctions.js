eb0980edd1a3fb6a7622d797274cf38e7368fa65
Wesley Jiang <wjiang917@gmail.com>
2024-06-06 09:04:44 -0600

    Added notesAPI

--- react_realestate/src/api/apiFunctions.js
+++ react_realestate/src/api/apiFunctions.js
@@ -8,6 +8,10 @@ const customerAPI = axios.create({
     baseURL: 'http://localhost:8000/customer/'
 })
 
+const notesAPI = axios.create({
+    baseURL: 'http://localhost:8000/notes/'
+})
+
 // Customer
 export const getAllCustomers = () => {
     return customerAPI.get('/');
@@ -23,4 +27,21 @@ export const putCustomer = (id, data) => {
 
 export const postCustomer = (data) => {
     return customerAPI.post('/', data);
+}
+
+// Notes
+export const postNote = (data) => {
+    return notesAPI.post('/', data);
+}
+
+export const getNotes = (id) => {
+    return notesAPI.get(`?content_type__app_label=api&content_type__model=customer&object_id=${id}`);
+}
+
+export const getNote = (id, noteID) => {
+    return notesAPI.get(`?content_type__app_label=api&content_type__model=customer&object_id=${id}&id=${noteID}`);
+}
+
+export const deleteNote = (id) => {
+    return notesAPI.delete(`/${id}/`);
 }
