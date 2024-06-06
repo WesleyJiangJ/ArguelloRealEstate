f9c7e43ac93f10ece8a0888213b5e94bdf8ce27c
Wesley Jiang <wjiang917@gmail.com>
2024-06-06 14:12:22 -0600

    Simplified the Sidebar by removing the SubMenu for Payments and adding a Sales menu item

--- react_realestate/src/components/Main/SideBar.jsx
+++ react_realestate/src/components/Main/SideBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import { HomeModernIcon, UserIcon, BanknotesIcon, UserGroupIcon, CogIcon, ArrowLeftStartOnRectangleIcon, CreditCardIcon, DocumentTextIcon } from "@heroicons/react/24/solid";
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { HomeModernIcon, UserIcon, BanknotesIcon, UserGroupIcon, CogIcon, ArrowLeftStartOnRectangleIcon } from "@heroicons/react/24/solid";
 
 export default function SideBar({ collapsed }) {
     const menuItemStyles = {
@@ -22,7 +22,7 @@ export default function SideBar({ collapsed }) {
                 collapsed={collapsed}
                 className='h-[100vh] bg-sidebar'>
                 <div className="mb-2 p-4 flex">
-                    <HomeModernIcon className='w-8 h-8 m-auto'/>
+                    <HomeModernIcon className='w-8 h-8 m-auto' />
                 </div>
                 <Menu menuItemStyles={menuItemStyles}>
                     <MenuItem
@@ -30,18 +30,11 @@ export default function SideBar({ collapsed }) {
                         component={<NavLink to="customer" style={getActiveStyles} />}>
                         Clientes
                     </MenuItem>
-                    <SubMenu label="Pagos" icon={<BanknotesIcon className="h-5 w-5" />}>
-                        <MenuItem
-                            icon={<CreditCardIcon className="h-5 w-5" />}
-                            component={<NavLink to="budget" style={getActiveStyles} />}>
-                            Presupuestos
-                        </MenuItem>
-                        <MenuItem
-                            icon={<DocumentTextIcon className="h-5 w-5" />}
-                            component={<NavLink to="payment" style={getActiveStyles} />}>
-                            Pagos
-                        </MenuItem>
-                    </SubMenu>
+                    <MenuItem
+                        icon={<BanknotesIcon className="h-5 w-5" />}
+                        component={<NavLink to="sales" style={getActiveStyles} />}>
+                        Ventas
+                    </MenuItem>
                 </Menu>
                 <Menu menuItemStyles={menuItemStyles}>
                     <MenuItem
