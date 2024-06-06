264f32fe06e96b6630aa48ac4099b7748db0a707
Wesley Jiang <wjiang917@gmail.com>
2024-06-06 09:04:16 -0600

    Added NotesViewSet with filtering capabilities

--- api/views.py
+++ api/views.py
@@ -1,8 +1,21 @@
 from rest_framework import viewsets
 from api.serializers import *
 from api.models import *
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+
+class NotesViewSet(viewsets.ModelViewSet):
+    queryset = Notes.objects.all()
+    serializer_class = NotesSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = {
+        "content_type__app_label": ["exact"],
+        "content_type__model": ["exact"],
+        "object_id": ["exact"],
+        "id": ["exact"],
+    }
