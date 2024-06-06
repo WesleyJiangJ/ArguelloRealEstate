7d8927ae559d73ba1bcc2d8ab9e3881b46f493bd
Wesley Jiang <wjiang917@gmail.com>
2024-06-06 09:02:25 -0600

    Added Notes model - Added polymorphic relationship for Notes in Customer model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,20 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+
+
+class Notes(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Fields for Polymorphic Relationship
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Customer(models.Model):
@@ -11,6 +26,7 @@ class Customer(models.Model):
     dni = models.CharField(max_length=16, validators=[MinLengthValidator(16)])
     phone_number = models.CharField(max_length=8)
     email = models.EmailField(max_length=255, blank=True)
+    notas = GenericRelation(Notes)
     status = models.BooleanField(default=True)
     modified_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
