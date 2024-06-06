49dfbf2006657a8b84505294021b14d9fe69f9dc
Wesley Jiang <wjiang917@gmail.com>
2024-06-06 10:12:08 -0600

    Added Personal model

--- api/models.py
+++ api/models.py
@@ -26,7 +26,25 @@ class Customer(models.Model):
     dni = models.CharField(max_length=16, validators=[MinLengthValidator(16)])
     phone_number = models.CharField(max_length=8)
     email = models.EmailField(max_length=255, blank=True)
-    notas = GenericRelation(Notes)
+    notes = GenericRelation(Notes)
+    status = models.BooleanField(default=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.middle_name} {self.first_surname} {self.second_surname}"
+
+
+class Personal(models.Model):
+    first_name = models.CharField(max_length=30)
+    middle_name = models.CharField(max_length=30)
+    first_surname = models.CharField(max_length=30)
+    second_surname = models.CharField(max_length=30)
+    birthdate = models.DateField()
+    dni = models.CharField(max_length=16, validators=[MinLengthValidator(16)])
+    phone_number = models.CharField(max_length=8)
+    email = models.EmailField(max_length=255, blank=True)
+    notes = GenericRelation(Notes)
     status = models.BooleanField(default=True)
     modified_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
