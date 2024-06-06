240210df99c2a6cbf2ccf6b0e82bbcf01e61c79e
Wesley Jiang <wjiang917@gmail.com>
2024-06-06 09:03:43 -0600

    Added routing for NotesViewSet to API URLs

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register(r"customer", views.CustomerViewSet)
+router.register(r"notes", views.NotesViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
