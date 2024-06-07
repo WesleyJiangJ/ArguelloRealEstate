e588bbdffe0e5656c57adc62a54d1d42e72c7e0f
Wesley Jiang <wjiang917@gmail.com>
2024-06-07 17:53:49 -0600

    Add Sales component with Table for displaying sales data

---
+++ react_realestate/src/components/Main/Sales.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import Table from "./Table";
+// import { getAllSales } from "../../api/apiFunctions";
+
+export default function Sales() {
+    const [salesData, setSalesData] = React.useState([]);
+    const INITIAL_VISIBLE_COLUMNS = ["full_name", "status"];
+    const columns = [
+        { name: "Nombres", uid: "full_name", sortable: true },
+        { name: "Celular", uid: "phone_number", sortable: true },
+        { name: "Estado", uid: "status", sortable: false },
+    ];
+    const statusColorMap = {
+        true: "success",
+        false: "danger",
+    };
+    const statusOptions = [
+        { name: "Activo", uid: true },
+        { name: "Inactivo", uid: false },
+    ];
+    const cellValues = [
+        {
+            firstColumn: "full_name",
+            firstValue: "`${item.first_name} ${item.middle_name} ${item.first_surname} ${item.second_surname}`",
+            secondValue: "`${item.email}`"
+        },
+        {
+            secondColum: "phone_number",
+            firstValue: "`${item.phone_number}`"
+        },
+        {
+            thirdColumn: "status",
+            firstValue: "`${item.status}`",
+            secondValue: {
+                first: 'Activo',
+                second: 'Inactivo'
+            }
+        }
+    ];
+    const sortedItem = {
+        first: "`${a.first_name} ${a.middle_name} ${a.first_lastname} ${a.second_lastname}`",
+        second: "`${b.first_name} ${b.middle_name} ${b.first_lastname} ${b.second_lastname}`"
+    }
+    const fetchData = async () => {
+        try {
+            // setSalesData((await getAllSales()).data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    React.useEffect(() => {
+        fetchData();
+    }, []);
+    return (
+        <>
+            <Table
+                value={"Ventas"}
+                showStatusDropdown={true}
+                showColumnsDropdown={true}
+                showAddButton={true}
+                typeOfData={"Transacciones"}
+                axiosResponse={salesData}
+                fetchData={fetchData}
+                INITIAL_VISIBLE_COLUMNS={INITIAL_VISIBLE_COLUMNS}
+                columns={columns}
+                statusColorMap={statusColorMap}
+                statusOptions={statusOptions}
+                cellValues={cellValues}
+                sortedItem={sortedItem}
+            />
+        </>
+    );
+}
