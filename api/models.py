adbb808935f8febfa05730a70be06e183c834f91
Wesley Jiang <wjiang917@gmail.com>
2024-06-07 17:45:14 -0600

    Plot and Sales models were added

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class Notes(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, blank=True)
     content = models.TextField(max_length=256)
     created_at = models.DateTimeField(auto_now_add=True)
     # Fields for Polymorphic Relationship
@@ -51,3 +51,29 @@ class Personal(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.middle_name} {self.first_surname} {self.second_surname}"
+
+
+class Plot(models.Model):
+    STATUS_CHOICES = [(0, "Available"), (1, "Process"), (2, "Sold")]
+    number = models.CharField(max_length=10, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    notes = GenericRelation(Notes)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=0)
+
+    def __str__(self):
+        return f"{self.number} - {self.status}"
+
+
+class Sale(models.Model):
+    STATUS_CHOICES = [(0, "Active"), (1, "Cancelled"), (2, "Completed")]
+    id_customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    id_personal = models.ForeignKey(Personal, on_delete=models.CASCADE)
+    id_plot = models.ForeignKey(Plot, on_delete=models.CASCADE)
+    premium = models.DecimalField(max_digits=10, decimal_places=2)
+    installment = models.PositiveIntegerField()
+    total_paid = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=0)
+    created_at = models.DateField()
+
+    def __str__(self):
+        return f"Plot {self.id_plot} sold to {self.id_customer} - {self.status}"
