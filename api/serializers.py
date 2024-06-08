c7ef40dfa6786ae2d0c8e7be71750ab8b276386b
Wesley Jiang <wjiang917@gmail.com>
2024-06-08 17:51:58 -0600

    Added to_representation function to SaleSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -30,3 +30,13 @@ class SaleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sale
         fields = "__all__"
+
+    def to_representation(self, instance):
+        representation = super(SaleSerializer, self).to_representation(instance)
+        customer_representation = CustomerSerializer(instance.id_customer).data
+        personal_representation = PersonalSerializer(instance.id_personal).data
+        plot_representation = PlotSerializer(instance.id_plot).data
+        representation["customer_data"] = customer_representation
+        representation["personal_data"] = personal_representation
+        representation["plot_data"] = plot_representation
+        return representation
