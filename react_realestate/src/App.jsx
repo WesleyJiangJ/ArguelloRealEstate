1daa13138d604b013ed0488ef118c842a955176e
Wesley Jiang <wjiang917@gmail.com>
2024-06-12 15:59:12 -0600

    Add Plot routes and update main context titles
    - Added new Plot routes in the application.
    - Updated MainContext titles to include 'plot'.
    - Reorganized routes to include Plot and its corresponding detail modal.

--- react_realestate/src/App.jsx
+++ react_realestate/src/App.jsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Main from './pages/Main';
 import Customer from './components/Main/Customer';
 import Detail from './components/Main/Detail';
+import Plot from './components/Main/Plot';
 import Personal from './components/Main/Personal';
 import Sales from './components/Main/Sales';
 import SalesDetail from './components/Main/SalesDetail';
@@ -14,6 +15,7 @@ function App() {
   const mainData = {
     titles: {
       customer: 'Clientes',
+      plot: 'Lotes',
       personal: 'Personal',
       sales: 'Ventas'
     },
@@ -26,13 +28,14 @@ function App() {
           <Route path='/main/*' element={<Main />}>
             <Route path='customer/*' element={<Customer />} />
             <Route path='customer/detail/:id' element={<Detail value={"Clientes"} />} />
+            <Route path='plot/*' element={<Plot />}>
+              <Route path='detail/:id' element={<PlotModal />} />
+            </Route>
             <Route path='personal/*' element={<Personal />} />
             <Route path='personal/detail/:id' element={<Detail value={"Personal"} />} />
             <Route path='sales/*' element={<Sales />} />
             <Route path='sales/detail/:id' element={<SalesDetail />} />
-            <Route path='settings/*' element={<Settings />}>
-              <Route path='plot/:id' element={<PlotModal />} />
-            </Route>
+            <Route path='settings/*' element={<Settings />} />
           </Route>
         </Routes>
       </MainContext.Provider>
