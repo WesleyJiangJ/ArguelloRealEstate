7ce71849bf96e06ef19ec84fce42d283988c240f
Wesley Jiang <wjiang917@gmail.com>
2024-06-13 18:41:09 -0600

    Create PDF.jsx component
    - Display the information about the sale as PDF
    - Generating an invoice
    - Added a download button to allow users to download the generated PDF

---
+++ react_realestate/src/components/Main/PDF.jsx
@@ -0,0 +1,192 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+import { getAllInstallmentByCustomer, getSpecificSale } from "../../api/apiFunctions"
+import { Page, Text, View, Document, Image, StyleSheet, PDFViewer, PDFDownloadLink } from '@react-pdf/renderer';
+import Logo from '../../assets/images/logo.png';
+import Stamp from '../../assets/images/stamp.png';
+import Tigo from '../../assets/images/tigo.png';
+import Claro from '../../assets/images/claro.png';
+import { saveAs } from 'file-saver';
+import { pdf } from '@react-pdf/renderer';
+import { Button } from '@nextui-org/react'
+
+export default function PDF() {
+    const param = useParams();
+    const [data, setData] = React.useState([]);
+    const [installment, setInstallment] = React.useState([]);
+    const styles = StyleSheet.create({
+        page: {
+            flexDirection: 'column',
+            backgroundColor: '#FFFFFF'
+        },
+        container: {
+            margin: 10,
+            padding: 10,
+        },
+        header: {
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            backgroundColor: "#F2F5F8"
+        },
+        text_title: {
+            fontSize: '20pt',
+            marginLeft: '20px',
+        },
+        text: {
+            fontSize: 16,
+            fontWeight: 'bold',
+            marginBottom: '15px'
+        },
+        text_date: {
+            fontSize: 12,
+            textAlign: 'right'
+        },
+        footer_text: {
+            fontSize: 14,
+            marginBottom: '10px'
+        },
+        mainContent: {
+            flex: 1
+        },
+        image: {
+            display: 'flex',
+            width: '250px',
+            margin: '20px'
+        }
+    });
+
+    React.useEffect(() => {
+        loadData();
+    }, []);
+
+    const loadData = async () => {
+        const res = (await getSpecificSale(param.id)).data;
+        setData(res);
+        setInstallment((await getAllInstallmentByCustomer(res.customer_data?.id, param.id)).data);
+    }
+
+    const currentDate = () => {
+        const fecha = new Date();
+        const opciones = { year: 'numeric', month: 'long', day: 'numeric' };
+        return fecha.toLocaleDateString('es-ES', opciones);
+    };
+
+    const MyPDF = () => (
+        <Document>
+            <Page size="A4" style={styles.page}>
+                <View style={styles.header}>
+                    <Image src={Logo} style={styles.image} />
+                    <Text style={styles.text_title}>Estado de Cuenta</Text>
+                </View>
+                <View style={styles.mainContent}>
+                    <View style={styles.container}>
+                        <Text style={styles.text_date}>
+                            {currentDate()}
+                        </Text>
+
+                        <Text style={styles.text}>
+                            {data.customer_data?.first_name} {data.customer_data?.middle_name} {data.customer_data?.first_surname} {data.customer_data?.second_surname}
+                        </Text>
+
+                        <Text style={styles.text}>
+                            {data.customer_data?.dni}
+                        </Text>
+
+                        <Text style={styles.text}>
+                            Lote N°{data.plot_data?.number}
+                        </Text>
+
+                        <Text style={styles.text}>
+                            {data.status === 0 ? 'Proceso de venta' : data.status === 1 ? 'Vendido' : 'Anulado'}
+                        </Text>
+
+                    </View>
+                    <View style={styles.container}>
+                        <Text style={[styles.text, { fontSize: 20, marginBottom: '20px', backgroundColor: '#F2F5F8', padding: '5px', borderRadius: '4px' }]}>
+                            Detalles
+                        </Text>
+
+                        <View style={[{ flexDirection: 'row', justifyContent: 'space-between' }]}>
+                            <Text style={[styles.text]}>
+                                Último mes abonado
+                            </Text>
+                            <Text style={[styles.text]}>
+                                {installment.length === 1 ? 'Se pagó la prima' : data.date_paid ? data.date_paid : 'Aún no se han realizado pagos'}
+                            </Text>
+                        </View>
+
+                        <View style={[{ flexDirection: 'row', justifyContent: 'space-between' }]}>
+                            <Text style={[styles.text]}>
+                                Cuotas pagadas
+                            </Text>
+                            <Text style={[styles.text]}>
+                                {installment.length === 1 ? 0 : installment.length - 1}
+                            </Text>
+                        </View>
+
+                        <View style={[{ flexDirection: 'row', justifyContent: 'space-between' }]}>
+                            <Text style={[styles.text]}>
+                                Precio del lote
+                            </Text>
+                            <Text style={[styles.text]}>
+                                {`$${parseFloat(data.plot_data?.price).toLocaleString()}`}
+                            </Text>
+                        </View>
+
+                        <View style={[{ flexDirection: 'row', justifyContent: 'space-between' }]}>
+                            <Text style={[styles.text]}>
+                                Total abonado
+                            </Text>
+                            <Text style={[styles.text]}>
+                                {`$${parseFloat(data.total_paid).toLocaleString()}`}
+                            </Text>
+                        </View>
+
+                        <View style={[{ flexDirection: 'row', justifyContent: 'space-between' }]}>
+                            <Text style={[styles.text]}>
+                                Pendiente
+                            </Text>
+                            <Text style={[styles.text]}>
+                                {`$${(parseFloat(data.plot_data?.price) - parseFloat(data.total_paid)).toLocaleString()}`}
+                            </Text>
+                        </View>
+
+                    </View>
+                    <View style={[{ flexDirection: 'column', marginTop: 'auto', padding: 10, backgroundColor: '#F2F5F8' }]}>
+                        <Text style={styles.footer_text}>Emitido por área de cobranza</Text>
+                        <Text style={styles.footer_text}>Este recibo respalda el pago reflejado en el comprobante del mes indicado</Text>
+                        <Text style={styles.footer_text}>Deposite a la cuenta bancaria N°:10020210106121 a nombre de Jorge Alberto Arguello Espinoza</Text>
+                        <View style={[{ flexDirection: 'row', justifyContent: 'space-between' }]}>
+                            <View style={[{ flexDirection: 'column' }]}>
+                                <Text style={styles.footer_text}>Oficina Central</Text>
+                                <View style={[{ flexDirection: 'row' }]}>
+                                    <Image src={Tigo} style={[{ marginRight: '5px', width: '15px', height: '15px' }]} />
+                                    <Text style={[styles.footer_text]}>+505 8544-1412</Text>
+                                </View>
+                                <View style={[{ flexDirection: 'row' }]}>
+                                    <Image src={Claro} style={[{ marginRight: '5px', width: '15px', height: '15px' }]} />
+                                    <Text style={[styles.footer_text]}>+505 8535-8435</Text>
+                                </View>
+                            </View>
+                            <Image src={Stamp} style={[{ width: '90px', height: '90px' }]} />
+                        </View>
+                    </View>
+                </View>
+            </Page>
+        </Document>
+    )
+    const downloadPdf = async () => {
+        const fileName = `${data.plot_data?.number}_${data.customer_data?.first_name}_${data.customer_data?.first_surname}_${data.date_paid}.pdf`;
+        const blob = await pdf(<MyPDF />).toBlob();
+        saveAs(blob, fileName);
+    };
+    return (
+        <>
+        <Button radius='sm' size='lg' className='bg-primary text-white my-2' fullWidth onClick={() => downloadPdf()}>Descargar</Button>
+        <PDFViewer className='w-full h-full'>
+            <MyPDF />
+        </PDFViewer>
+        </>
+    );
+}
