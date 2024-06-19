b9756a8a29adcb6a2dc3ea632573b2f2672eba87
Wesley Jiang <wjiang917@gmail.com>
2024-06-19 10:25:28 -0600

    Added sorting by date functionality

--- react_realestate/src/components/Main/Sales.jsx
+++ react_realestate/src/components/Main/Sales.jsx
@@ -4,10 +4,10 @@ import { getAllSales } from "../../api/apiFunctions";
 
 export default function Sales() {
     const [salesData, setSalesData] = React.useState([]);
-    const INITIAL_VISIBLE_COLUMNS = ["full_name", "plot", "status"];
+    const INITIAL_VISIBLE_COLUMNS = ["full_name", "date_paid", "status"];
     const columns = [
         { name: "Nombres", uid: "full_name", sortable: false },
-        { name: "Lote", uid: "plot", sortable: false },
+        { name: "Último Pago", uid: "date_paid", sortable: false },
         { name: "Estado", uid: "status", sortable: false },
     ];
     const statusColorMap = {
@@ -22,11 +22,11 @@ export default function Sales() {
         {
             firstColumn: "full_name",
             firstValue: "`${item.customer_data.first_name} ${item.customer_data.middle_name} ${item.customer_data.first_surname} ${item.customer_data.second_surname}`",
-            secondValue: "``"
+            secondValue: "`${item.plot_data.number}`"
         },
         {
-            secondColumn: "plot",
-            firstValue: "`${item.plot_data.number}`"
+            secondColumn: "date_paid",
+            firstValue: "`${formatDate(item.date_paid)}`"
         },
         {
             thirdColumn: "status",
@@ -38,12 +38,12 @@ export default function Sales() {
         }
     ];
     const sortedItem = {
-        first: "`${a.customer_data.first_name}`",
-        second: "`${b.customer_data.first_name}`"
+        first: "``",
+        second: "``"
     }
     const fetchData = async () => {
         try {
-            setSalesData((await getAllSales()).data.filter((data) => data.status === 0 || data.status === 1));
+            setSalesData((await getAllSales()).data.filter((data) => data.status === 0).sort((a, b) => new Date(a.date_paid) - new Date(b.date_paid)));
         } catch (error) {
             console.error("Error fetching data:", error);
         }
