5bdaf851c27d53bdb14f19cc3e17d4ea231d9896
Wesley Jiang <wjiang917@gmail.com>
2024-06-22 16:59:21 -0600

    feat: Add Penalty component to handle penalty-related data and display
    - Import necessary functions and components for penalty handling.
    - Add Penalty component to display and manage penalty data, including history and payments.
    - Use useForm for form handling and data submission.
    - Implement loadData function to fetch penalty data, history, and payments.
    - Add form for submitting penalty payments with validation.
    
    This commit adds the Penalty component to manage and display penalty-related data for sales.

---
+++ react_realestate/src/components/Main/Penalty.jsx
@@ -0,0 +1,194 @@
+import React from "react";
+import { useForm, Controller } from "react-hook-form"
+import { getPenalty, getPenaltyHistory, getPenaltyPayment, patchPenalty, postPenaltyPayment } from "../../api/apiFunctions";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Input, Select, SelectItem, Tab, Tabs, Card, CardBody } from "@nextui-org/react";
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+export default function Penalty({ isOpen, onOpenChange, id_sale }) {
+    const [penaltyData, setPenaltyData] = React.useState([]);
+    const [penaltyHistory, setPenaltyHistory] = React.useState([]);
+    const [penaltyPayment, setPenaltyPayment] = React.useState([]);
+    const { control, handleSubmit, formState: { errors }, reset, setValue } = useForm({
+        defaultValues: {
+            id_penalty: '',
+            amount: '',
+            payment_type: 0
+        }
+    });
+
+    React.useEffect(() => {
+        loadData();
+    }, [penaltyHistory]);
+
+    const loadData = async () => {
+        const resPenalty = (await getPenalty(id_sale)).data;
+        if (resPenalty) {
+            setPenaltyData(resPenalty);
+            setPenaltyHistory((await getPenaltyHistory(resPenalty[0].id)).data.sort((a, b) => new Date(a.date) - new Date(b.date)));
+            setPenaltyPayment((await getPenaltyPayment(resPenalty[0].id)).data);
+        }
+    }
+    const columns = [
+        { name: "Mes", uid: "month" },
+        { name: "Mensualidad", uid: "monthly_installments" },
+        { name: "Pago", uid: "monthly_amount" },
+        { name: "Mora", uid: "penalty" },
+    ];
+    const renderCell = React.useCallback((data, columnKey) => {
+        const cellValue = data[columnKey];
+        switch (columnKey) {
+            case "month":
+                return (
+                    <p className="text-bold text-small capitalize">{data.date}</p>
+                );
+            case "monthly_installments":
+                return (
+                    <p className="text-bold text-small capitalize">${parseFloat(data.monthly_debt).toLocaleString()}</p>
+                );
+            case "monthly_amount":
+                return (
+                    <p className="text-bold text-small capitalize">${parseFloat(data.total_debt).toLocaleString()}</p>
+                );
+            case "penalty":
+                return (
+                    <p className="text-bold text-small capitalize">${data.penalty}</p>
+                );
+            default:
+                return cellValue;
+        }
+    }, []);
+
+    const onSubmit = async (data) => {
+        data.id_penalty = penaltyData[0].id;
+        await postPenaltyPayment(data).then(async () => {
+            await patchPenalty(penaltyData[0].id, { total: parseFloat(penaltyData[0].total) - parseFloat(data.amount) }).then(() => {
+                reset();
+                loadData();
+            });
+        });
+    }
+    return (
+        <>
+            <Modal
+                radius="sm"
+                size="5xl"
+                backdrop="blur"
+                isOpen={isOpen}
+                onOpenChange={onOpenChange}>
+                <ModalContent>
+                    {(onClose) => (
+                        <>
+                            <ModalHeader className="flex flex-col gap-2">Mora</ModalHeader>
+                            <ModalBody>
+                                <form onSubmit={handleSubmit(onSubmit)}>
+                                    <div className="flex flex-row gap-2 items-center">
+                                        <Input label="Mora Total" variant="underlined" isReadOnly value={`$${penaltyData[0].total}`} />
+                                        <Controller
+                                            name="amount"
+                                            control={control}
+                                            rules={{ required: true }}
+                                            render={({ field }) => (
+                                                <Input
+                                                    {...field}
+                                                    label='Monto a pagar'
+                                                    variant="underlined"
+                                                    startContent={'$'}
+                                                    placeholder="0.00"
+                                                    isInvalid={errors.amount ? true : false}
+                                                />
+                                            )}
+                                        />
+                                        <Controller
+                                            name="payment_type"
+                                            control={control}
+                                            rules={{ required: true }}
+                                            render={({ field }) => (
+                                                <Select
+                                                    {...field}
+                                                    label="Tipo"
+                                                    defaultSelectedKeys={'0'}
+                                                    radius="sm"
+                                                    variant="underlined"
+                                                    disallowEmptySelection
+                                                    onChange={(e) => {
+                                                        field.onChange(e);
+                                                    }}>
+                                                    <SelectItem key={0} value={0}>Pago</SelectItem>
+                                                    <SelectItem key={1} value={1}>Exoneración</SelectItem>
+                                                </Select>
+                                            )}
+                                        />
+                                        <Button color="primary" radius="sm" size="lg" isIconOnly type="submit">
+                                            <PlusIcon className="w-5 h-5" />
+                                        </Button>
+                                    </div>
+                                </form>
+                                <Tabs aria-label="Options" fullWidth radius="sm" color="primary">
+                                    <Tab key="paid" title="Pagado">
+                                        <Card shadow="none" radius="sm" className="h-96">
+                                            <CardBody>
+                                                <Table
+                                                    aria-label="Penalty Table"
+                                                    radius="sm"
+                                                    shadow="none"
+                                                    className="flex-grow overflow-auto">
+                                                    <TableHeader>
+                                                        <TableColumn>Monto Pagado</TableColumn>
+                                                        <TableColumn>Tipo</TableColumn>
+                                                    </TableHeader>
+                                                    <TableBody emptyContent={"No hubieron resultados"}>
+                                                        {penaltyPayment.map((row) =>
+                                                            <TableRow key={row.id}>
+                                                                <TableCell>${row.amount}</TableCell>
+                                                                <TableCell>{row.payment_type === 0 ? 'Pago' : 'Exoneración'}</TableCell>
+                                                            </TableRow>
+                                                        )}
+                                                    </TableBody>
+                                                </Table>
+                                            </CardBody>
+                                        </Card>
+                                    </Tab>
+                                    {/* <Tab key="details" title="Detalles">
+                                        <Card shadow="none" radius="sm" className="h-96">
+                                            <CardBody>
+                                                <Table
+                                                    aria-label="Penalty Table"
+                                                    radius="sm"
+                                                    shadow="none"
+                                                    className="flex-grow overflow-auto">
+                                                    <TableHeader columns={columns}>
+                                                        {(column) => (
+                                                            <TableColumn key={column.uid}>
+                                                                {column.name}
+                                                            </TableColumn>
+                                                        )}
+                                                    </TableHeader>
+                                                    <TableBody emptyContent={"No hubieron resultados"} items={penaltyHistory}>
+                                                        {(item) => (
+                                                            <TableRow key={item.month}>
+                                                                {(columnKey) =>
+                                                                    <TableCell>
+                                                                        {renderCell(item, columnKey)}
+                                                                    </TableCell>
+                                                                }
+                                                            </TableRow>
+                                                        )}
+                                                    </TableBody>
+                                                </Table>
+                                            </CardBody>
+                                        </Card>
+                                    </Tab> */}
+                                </Tabs>
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="danger" variant="light" radius="sm" onPress={onClose}>
+                                    Cerrar
+                                </Button>
+                            </ModalFooter>
+                        </>
+                    )}
+                </ModalContent>
+            </Modal >
+        </>
+    );
+}
