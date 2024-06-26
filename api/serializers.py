db2e50208fa0afe80c265bfd8939e0565602213f
Wesley Jiang <wjiang917@gmail.com>
2024-06-26 14:17:58 -0600

    Add UserSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from api.models import *
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
 
 
 class CustomerSerializer(serializers.ModelSerializer):
