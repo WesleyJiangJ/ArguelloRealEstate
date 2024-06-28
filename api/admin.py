c1f3e2ad1f589014b6a3756aadd4be65ea9385c6
Wesley Jiang <wjiang917@gmail.com>
2024-06-28 13:46:22 -0600

    Register models in Django admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from api import models
 
-# Register your models here.
+admin.site.register(models.Customer)
+admin.site.register(models.Personal)
+admin.site.register(models.Notes)
+admin.site.register(models.Plot)
+admin.site.register(models.Sale)
+admin.site.register(models.Installment)
+admin.site.register(models.Commission)
+admin.site.register(models.Penalty)
+admin.site.register(models.PenaltyHistory)
+admin.site.register(models.PenaltyPayments)
+admin.site.register(models.PDFInformation)
