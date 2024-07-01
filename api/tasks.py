e7e7c9b7e30f335db64c657b0849487110ae955c
Wesley Jiang <wjiang917@gmail.com>
2024-07-01 14:17:20 -0600

    Organized imports and maintained code structure

--- api/tasks.py
+++ api/tasks.py
@@ -1,14 +1,13 @@
 import os
 import sys
 import django
+import schedule
+import time
+from views import send_database
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "RealEstate.settings")
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 django.setup()
-
-import schedule
-import time
-from views import send_database
 
 schedule.every(3).days.do(send_database)
 
