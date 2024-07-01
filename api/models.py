fdd04276154e4866a3201d5b3f7f5aa93991d121
Wesley Jiang <wjiang917@gmail.com>
2024-07-01 10:35:22 -0600

    Model refactor: - Removed 'app_password' field from Personal and added it to PDFInformation with password encryption. - Modified 'email' field in Personal to be blankable. - Removed 'save' method from Personal.

--- api/models.py
+++ api/models.py
@@ -48,25 +48,14 @@ class Personal(models.Model):
     birthdate = models.DateField()
     dni = models.CharField(max_length=16, validators=[MinLengthValidator(16)])
     phone_number = models.CharField(max_length=8)
-    email = models.EmailField(max_length=255, blank=False)
+    email = models.EmailField(max_length=255, blank=True)
     note = GenericRelation(Notes)
-    app_password = models.CharField(max_length=256)
     status = models.BooleanField(default=True)
     modified_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.first_name} {self.middle_name} {self.first_surname} {self.second_surname}"
-
-    def save(self, *args, **kwargs):
-        # Encrypt your password if it's new or changed
-        if self.pk is None:  # New object
-            self.app_password = make_password(self.app_password)
-        else:
-            old_instance = Personal.objects.get(pk=self.pk)
-            if self.app_password != old_instance.app_password:
-                self.app_password = make_password(self.app_password)
-        super().save(*args, **kwargs)
 
 
 @receiver(post_save, sender=Personal)
@@ -165,3 +154,15 @@ class PDFInformation(models.Model):
     phone_number_one_company = models.CharField(max_length=1, choices=TYPE_CHOICES)
     phone_number_two = models.CharField(max_length=8)
     phone_number_two_company = models.CharField(max_length=1, choices=TYPE_CHOICES)
+    backup_email = models.EmailField(max_length=255, blank=False)
+    app_password = models.CharField(max_length=256)
+
+    def save(self, *args, **kwargs):
+        # Encrypt your password if it's new or changed
+        if self.pk is None:  # New object
+            self.app_password = make_password(self.app_password)
+        else:
+            old_instance = PDFInformation.objects.get(pk=self.pk)
+            if self.app_password != old_instance.app_password:
+                self.app_password = make_password(self.app_password)
+        super().save(*args, **kwargs)
