14a0d746dee8d56a3b0141bd9fcca8e219016dd6
Wesley Jiang <wjiang917@gmail.com>
2024-09-26 16:22:47 -0600

    Configure Vite server to allow access from external hosts

--- react_realestate/vite.config.js
+++ react_realestate/vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [react()],
   css: {
     postcss: {
